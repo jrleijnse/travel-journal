3a676b55ac17f6a24d99d9717c7f33b2000a3c2c
Jeroen Leijnse <jr_leijnse@live.nl>
2023-03-27 11:15:22 +0200

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="header">
-      <ion-icon src="src/images/earth.svg"></ion-icon>
+      <ion-icon src="./src/images/earth.svg"></ion-icon>
       <span className="header-title">my travel journal.</span>
     </div>
   );
