b3605fffdddf0fc7c17e3b9e1488f53d4b4ce05a
Jeroen Leijnse <jr_leijnse@live.nl>
2023-03-27 11:37:16 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="header">
-      <ion-icon src="https://raw.githubusercontent.com/jrleijnse/travel-journal/images/earth.svg?example=foo&sanitize=true>"></ion-icon>
+      <ion-icon src="https://raw.githubusercontent.com/jrleijnse/travel-journal/src/images/earth.svg"></ion-icon>
       <span className="header-title">my travel journal.</span>
     </div>
   );
